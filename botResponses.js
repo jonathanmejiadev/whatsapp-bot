dec7869a5bf8634e14d3ac7b96945eae13a6c23b
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-07-05 20:37:58 -0300

    update bot responses

--- botResponses.js
+++ botResponses.js
@@ -1,7 +1,5 @@
 const botResponses = {
-    'hola': `Hola, buenas tardes! \n 
-    Jonathan no esta disponible en este momento, pero puedes hablar conmigo si quieres, 
-    pregúntame como estoy y te contestare :)`,
+    'hola': `Hola, como estas? Jonathan no esta disponible en este momento, pero puedes hablar conmigo si quieres, pregúntame como estoy y te contestare :)`,
     'como': `Yo estoy muy bien, espero que tú también.`,
     'adios': `Hasta luego!`,
 };
