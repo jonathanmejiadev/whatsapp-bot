72cefce8b21544d843801343e7c429ad1043ee40
Jonathan Mejia <jonathanmejia.ar@gmail.com>
2021-07-05 20:30:29 -0300

    save client history in a excel file

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const { Client } = require('whatsapp-web.js');
+const { Client, MessageMedia } = require('whatsapp-web.js');
 const qrcode = require('qrcode-terminal');
 const fs = require('fs');
 const ora = require('ora');
 const chalk = require('chalk');
 const botResponses = require('./botResponses');
-
+const exceljs = require('exceljs');
+const moment = require('moment');
 
 const SESSION_FILE_PATH = './session.json';
 let sessionData;
@@ -61,12 +62,58 @@ const listenMessage = () => {
         body = body.split(' ').find(word => botResponses[word]);
         let response = botResponses[body] || 'El robot no puede responder a eso, vuelve a intentarlo.';
         sendMessage(from, response);
-        console.log(`${chalk.yellow(body, from, to)}`);
+        saveHistorial(from, msg.body);
+
+        console.log(`${chalk.yellow(msg.body, from, to)}`);
     });
 };
 
 const sendMessage = (to, msg) => {
     client.sendMessage(to, msg);
+};
+
+const sendMedia = (to, file) => {
+    const mediaFile = MessageMedia.fromFilePath(`./media/${file}`);
+    client.sendMessage(to, mediaFile);
+};
+
+//save number and message in a excel file
+const saveHistorial = (number, msg) => {
+    const pathChat = `./chats/${number}.xlsx`;
+    const workbook = new exceljs.Workbook();
+    const today = moment().format('DD-MM-YYYY hh:mm');
+
+    if (fs.existsSync(pathChat)) {
+        workbook.xlsx.read(pathChat)
+            .then(() => {
+                const worksheet = workbook.getWorksheet(1);
+                const lastRow = worksheet.lastRow;
+                let getRowInsert = worksheet.getRow(++(lastRow.number));
+                getRowInsert.getCell('A').value = today;
+                getRowInsert.getCell('A').value = msg;
+                getRowInsert.commit();
+                worksheet.xlsx.writeFile(pathChat)
+                    .then(() => {
+                        console.log('Chat added!')
+                    }).catch(() => {
+                        console.log('Something went wrong!')
+                    });
+            });
+    } else {
+        //create a xlsx file with phone number
+        const worksheet = workbook.addWorksheet('Chats');
+        worksheet.columns = [
+            { header: 'Fecha', key: 'date' },
+            { header: 'Mensaje', key: 'message' }
+        ];
+        worksheet.addRow([today, msg]);
+        workbook.xlsx.writeFile(pathChat)
+            .then(() => {
+                console.log('History created!')
+            }).catch(() => {
+                console.log('Something went wrong!');
+            });
+    };
 };
 
 (fs.existsSync(SESSION_FILE_PATH)) ? withSession() : withoutSession();
